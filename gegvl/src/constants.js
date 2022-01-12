a67527e621cc0f786bf12c7ca1f83281a1a13395
jackcsheehan <jackcsheehan@users.noreply.github.com>
2022-01-12 14:22:27 -0600

    Django setup

--- gegvl/src/constants.js
+++
@@ -1,7 +0,0 @@
-// Colors
-export const OCEAN_COLOR = [138, 161, 255];
-export const LAND_COLOR = [255, 248, 224];
-
-// Local file paths
-export const OCEAN_DATA_PATH = "assets/geodata/oceans.geojson";
-export const LAND_DATA_PATH = "assets/geodata/land.geojson"
