ee850c142ab853cd19c540929447d0b5dad5f4be
jackcsheehan <jackcsheehan@users.noreply.github.com>
2022-01-12 11:00:09 -0600

    deck.gl setup

---
+++ gegvl/src/components/Map.js
@@ -0,0 +1,37 @@
+import React from "react";
+import DeckGL from "@deck.gl/react";
+import { GeoJsonLayer } from "deck.gl";
+import * as Constants from "../constants.js";
+
+const INITIAL_VIEW_STATE = {
+    longitude: -96.7970,
+    latitude: 32.7767,
+    zoom: 5,
+    pitch: 0,
+    bearing: 0,
+};
+
+// Component to manage a deck.gl map
+export default function Map() {
+    const layers = [
+        new GeoJsonLayer({
+            id: "oceans",
+            data: Constants.OCEAN_DATA_PATH,
+            filled: true,
+            getFillColor: Constants.OCEAN_COLOR
+        }),
+        new GeoJsonLayer({
+            id: "land",
+            data: Constants.LAND_DATA_PATH,
+            filled: true,
+            getFillColor: Constants.LAND_COLOR
+        }),
+    ];
+    return (
+        <DeckGL
+            controller={true}
+            initialViewState={INITIAL_VIEW_STATE}
+            layers={layers}
+        />
+    )
+}
